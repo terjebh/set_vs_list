8b84246bd8342fb9b6bd9a3aafa511d640492387
terjebh <terje@itfakutetet.no>
2024-04-01 15:37:28 +0200

    Eksempel på effektiviteten til set vs list

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
     int N = 50000;
-    List<Integer> tallListe = new ArrayList<>();
+    List<Integer> tallListe = new LinkedList<>();
     for(int i=0;i<N; i++) tallListe.add(i);
-    Collections.shuffle(tallListe);
-
+    //Collections.shuffle(tallListe);
+    Collections.sort(tallListe);
     Set<Integer> tallSett = new HashSet<>(tallListe);
 
         System.out.println(STR."Fjerne \{N} tall i Arraylist: \{fjern(tallListe)} millisekunder");
